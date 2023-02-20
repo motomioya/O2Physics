8b2f9c76977c7bd5f04464e65601033191e3b003
motomioya <motomi.oya@cern.ch>
2023-02-21 07:12:02 +0900

    Add mftmchmatchinginfo workflow

---
+++ mooya/src/mftmchmatchinginfo.cxx
@@ -0,0 +1,147 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+// Made by mooya
+
+#include "Common/DataModel/TrackSelectionTables.h"
+#include "Framework/AnalysisDataModel.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/runDataProcessing.h"
+#include "ReconstructionDataFormats/TrackFwd.h"
+#include "Math/SMatrix.h"
+#include "DetectorsBase/Propagator.h"
+#include "MFTTracking/Tracker.h"
+#include "Common/Core/trackUtilities.h"
+#include "CommonUtils/NameConf.h"
+#include "Common/Core/TrackSelection.h"
+#include "Common/DataModel/TrackSelectionTables.h"
+
+using namespace o2;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+
+using SMatrix55 = ROOT::Math::SMatrix<double, 5, 5, ROOT::Math::MatRepSym<double, 5>>;
+using SMatrix5 = ROOT::Math::SVector<double, 5>;
+using o2::track::TrackParCovFwd;
+using o2::track::TrackParFwd;
+
+
+struct mftmchmatchinginfo {
+
+  HistogramRegistry registry{
+    "registry",
+    {},
+    OutputObjHandlingPolicy::AnalysisObject,
+    true,
+    true};
+
+  void init(o2::framework::InitContext&)
+  {
+    AxisSpec trackXPos = {100, -50, 50, "X in cm"};
+    AxisSpec trackYPos = {100, -50, 50, "Y in cm"};
+    AxisSpec trackZPos = {120, -100, 20, "Z in cm"};
+
+    //HistogramConfigSpec HistVariable({HistType::kTHnSparseF, {ptRecoAxis, dcaxAxis, dcaxAxis, dcaAxis, zvtxAxis}});
+    registry.add("MFTXYPos", "MFTTrack XYPosition", {HistType::kTH2F, {trackXPos, trackYPos}});
+    registry.add("MFTZPos", "MFT ZPosition", {HistType::kTH1F, {trackZPos}});
+    registry.add("MUONXYPos", "MUONTrack XYPosition", {HistType::kTH2F, {trackXPos, trackYPos}});
+    registry.add("MUONZPos", "MUONTrack ZPosition", {HistType::kTH1F, {trackZPos}});
+    registry.add("MFTXYPosProp", "MFTTrack XYPosition at Last MFT Disk", {HistType::kTH2F, {trackXPos, trackYPos}});
+    registry.add("MFTZPosProp", "MFT ZPosition at Last MFT Disk", {HistType::kTH1F, {trackZPos}});
+    registry.add("MUONXYPosProp", "MUONTrack XYPosition at Last MFT Disk", {HistType::kTH2F, {trackXPos, trackYPos}});
+    registry.add("MUONZPosProp", "MUONTrack ZPosition at Last MFT Disk", {HistType::kTH1F, {trackZPos}});
+    registry.add("MCHMFTDisp", "Displacement MUONTrack and MFTTrack in XY plane", {HistType::kTH2F, {trackXPos, trackYPos}});
+  }
+
+  void process(aod::Collisions::iterator const& collision, aod::FwdTracks const& tracks, aod::MFTTracks const& mfttracks)
+  {
+    static constexpr Double_t sLastMFTPlaneZ = o2::mft::constants::mft::LayerZCoordinate()[9];
+    float mBz = -5.f;
+
+    int numberGlobalMuonTrack = 0;
+
+    //count the number of globalmuontrack to create a table;
+    for (auto const& track : tracks) {
+      if (track.has_collision()) {
+        if (track.trackType() == o2::aod::fwdtrack::ForwardTrackTypeEnum::GlobalMuonTrack) {
+		numberGlobalMuonTrack++;
+        }
+      }
+    }
+
+    double mchmftpair[numberGlobalMuonTrack][2];
+
+    int counttracknumber = 0;
+
+    //create a pair of MFTTrack and MUONTrack in same globalmuontrack
+    for (auto const& track : tracks) {
+      if (track.has_collision()) {
+        if (track.trackType() == o2::aod::fwdtrack::ForwardTrackTypeEnum::GlobalMuonTrack) {
+	  mchmftpair[counttracknumber][0] = track.matchMCHTrackId();
+	  mchmftpair[counttracknumber][1] = track.matchMFTTrackId();
+
+	  counttracknumber++;
+        }
+      }
+    }
+
+    for (auto const& track : tracks) {
+      if (track.has_collision()) {
+        if (track.trackType() == o2::aod::fwdtrack::ForwardTrackTypeEnum::MuonStandaloneTrack) {
+	  for (int i = 0; i<numberGlobalMuonTrack; i++){
+	    if (track.globalIndex() == mchmftpair[i][0]){
+	      for (auto const& mfttrack : mfttracks) {
+	        if (mfttrack.globalIndex() == mchmftpair[i][1]){
+
+		  registry.fill(HIST("MUONXYPos"), track.x(), track.y());
+		  registry.fill(HIST("MUONZPos"), track.z());
+		  registry.fill(HIST("MFTXYPos"), mfttrack.x(), mfttrack.y());
+		  registry.fill(HIST("MFTZPos"), mfttrack.z());
+
+                  //propagate muontrack to matching position
+                  double muonchi2 = track.chi2();
+                  SMatrix5 muonpars(track.x(), track.y(), track.phi(), track.tgl(), track.signed1Pt());
+                  std::vector<double> muonv1;
+                  SMatrix55 muoncovs(muonv1.begin(), muonv1.end());
+                  o2::track::TrackParCovFwd muonpars1{track.z(), muonpars, muoncovs, muonchi2};
+                  muonpars1.propagateToZ(sLastMFTPlaneZ,mBz);
+		  registry.fill(HIST("MUONXYPosProp"), muonpars1.getX(), muonpars1.getY());
+		  registry.fill(HIST("MUONZPosProp"), muonpars1.getZ());
+
+                  //propagate mfttrack to matching position
+                  double mftchi2 = mfttrack.chi2();
+                  SMatrix5 mftpars(mfttrack.x(), mfttrack.y(), mfttrack.phi(), mfttrack.tgl(), mfttrack.signed1Pt());
+                  std::vector<double> mftv1;
+                  SMatrix55 mftcovs(mftv1.begin(), mftv1.end());
+                  o2::track::TrackParCovFwd mftpars1{mfttrack.z(), mftpars, mftcovs, mftchi2};
+                  mftpars1.propagateToZ(sLastMFTPlaneZ,mBz);
+		  registry.fill(HIST("MFTXYPosProp"), mftpars1.getX(), mftpars1.getY());
+		  registry.fill(HIST("MFTZPosProp"), mftpars1.getZ());
+
+		  //Plot displacement
+		  double disX = muonpars1.getX() - mftpars1.getX();
+		  double disY = muonpars1.getY() - mftpars1.getY();
+		  registry.fill(HIST("MCHMFTDisp"), disX, disY);
+		}
+	      }
+	    }
+	  }
+        }
+      }
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<mftmchmatchinginfo>(cfgc),
+  };
+}
